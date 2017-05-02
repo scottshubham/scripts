67ca7fad9a2f76431d57b86be8d59573669a0c20
scottshubham <scott.shubham@ithands.biz>
2017-05-02 10:37:35 +0530

    Script to remove all Virtual hosts

---
+++ ri.sh
@@ -0,0 +1,3 @@
+sudo a2dissite
+*
+
--- rr.sh
+++ rr.sh
@@ -1,15 +1,16 @@
 # This will get the list of all the sites available and then disable them
 ls -1 /etc/apache2/sites-enabled > se.txt
-echo "Do you want to disable all the sites(y/n)?"
-read x
-if [ "$x" "=" "y" ]
-then
-    while read name
-    do
-        a2dissite $name
-    done < se.txt
-    service apache2 restart
-    echo "The request has been proceeded! Thank you!"
-else
-    echo "The request has been canceled! Thank you!"
-fi
+while read name
+do
+    a2dissite $name
+done < se.txt
+x="$(service apache2 restart)"
+echo "$$$"
+echo $x
+echo "$$$"
+# if [[ $x =~ [ * Restarting web server apache2 * fail! ] ]]
+# then
+#     echo "The request has been proceeded! Thank you!"
+# else
+#     echo "Try adding sudo to your command! Some commands in this script require sudo!"
+# fi
